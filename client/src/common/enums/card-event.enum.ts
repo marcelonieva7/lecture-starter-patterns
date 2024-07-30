149aa11f8c88004e737d35e96b8dc2f35596d369
Marcelo Nieva <marcelonieva7@gmail.com>
2024-07-29 22:04:52 -0300

    feat(client): add card events

--- client/src/common/enums/card-event.enum.ts
+++ client/src/common/enums/card-event.enum.ts
@@ -3,6 +3,8 @@ const CardEvent = {
   REORDER: "card:reorder",
   RENAME: "card:rename",
   CHANGE_DESCRIPTION: "card:change-description",
+  DELETE: "card:delete",
+  CLONE: "card:clone"
 } as const;
 
 export { CardEvent };
